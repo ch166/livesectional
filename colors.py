7de00359e18954e1f99e150592da0465e2e80fb5
Chris Higgins <chris.higgins@alternoc.net>
2022-03-12 10:08:59 -0800

    reformatted using black - colors.py

--- colors.py
+++ colors.py
@@ -9,64 +9,67 @@ def rgb2hex(rgb):
     """Convert RGB to HEX"""
     debugging.dprint(rgb)
     (red_value, green_value, blue_value) = rgb
-    hexval = '#%02x%02x%02x' % (red_value, green_value, blue_value)
+    hexval = "#%02x%02x%02x" % (red_value, green_value, blue_value)
     return hexval
 
 
 def hex2rgb(conf, value):
     """Hex to RGB"""
     # TODO: Add checks here to error if we get a non HEX value
-    value = value.lstrip('#')
+    value = value.lstrip("#")
     length_v = len(value)
-    return tuple(int(value[i:i+length_v//3], 16)
-                 for i in range(0, length_v, length_v//3))
+    return tuple(
+        int(value[i : i + length_v // 3], 16) for i in range(0, length_v, length_v // 3)
+    )
+
 
 def black(conf):
-    """ Return HEX Color code for Black """
+    """Return HEX Color code for Black"""
     return "#000000"
 
 
 def HEX_tuple(col_tuple):
     return HEX(col_tuple[0], col_tuple[1], col_tuple[2])
 
+
 def HEX(red_value, green_value, blue_value):
-    """ Return HEX values from RGB string """
-    hexval = '#%02x%02x%02x' % (red_value, green_value, blue_value)
+    """Return HEX values from RGB string"""
+    hexval = "#%02x%02x%02x" % (red_value, green_value, blue_value)
     return hexval
 
 
 def RGB(conf, value):
-    """ Return RGB values from HEX string """
+    """Return RGB values from HEX string"""
     return hex2rgb(conf, value)
 
 
 def VFR(conf):
-    """ Get VFR Color code from config """
+    """Get VFR Color code from config"""
     return conf.get_color("colors", "color_vfr")
 
 
 def MVFR(conf):
-    """ Get MVFR Color code from config """
+    """Get MVFR Color code from config"""
     return conf.get_color("colors", "color_mvfr")
 
 
 def IFR(conf):
-    """ Get IFR Color code from config """
+    """Get IFR Color code from config"""
     return conf.get_color("colors", "color_ifr")
 
 
 def LIFR(conf):
-    """ Get LIFR Color code from config """
+    """Get LIFR Color code from config"""
     return conf.get_color("colors", "color_lifr")
 
 
 def LIGHTNING(conf):
-    """ Get Lightning Color code from config """
+    """Get Lightning Color code from config"""
     return conf.get_color("colors", "color_lghtn")
 
 
 def SNOW(conf, value):
-    """ Get SNOW Color code from config """
+    """Get SNOW Color code from config"""
     if value == 1:
         return conf.get_color("colors", "color_snow1")
     else:
@@ -74,7 +77,7 @@ def SNOW(conf, value):
 
 
 def FRZRAIN(conf, value):
-    """ Get Freezing Rain Color code from config """
+    """Get Freezing Rain Color code from config"""
     if value == 1:
         return conf.get_color("colors", "color_frrain1")
     else:
@@ -82,7 +85,7 @@ def FRZRAIN(conf, value):
 
 
 def DUST_SAND_ASH(conf, value):
-    """ Get Dust Sand Ash Color (1) code from config """
+    """Get Dust Sand Ash Color (1) code from config"""
     if value == 1:
         return conf.get_color("colors", "color_dustsandash1")
     else:
@@ -90,7 +93,7 @@ def DUST_SAND_ASH(conf, value):
 
 
 def FOG(conf, value):
-    """ Get FOG Color code from config """
+    """Get FOG Color code from config"""
     if value == 1:
         return conf.get_color("colors", "color_fog1")
     else:
@@ -98,7 +101,7 @@ def FOG(conf, value):
 
 
 def RAIN(conf, value):
-    """ Get RAIN Color code from config """
+    """Get RAIN Color code from config"""
     if value == 1:
         return conf.get_color("colors", "color_rain1")
     else:
@@ -106,8 +109,5 @@ def RAIN(conf, value):
 
 
 def NOWEATHER(conf):
-    """ Get NOWX Color code from config """
+    """Get NOWX Color code from config"""
     return conf.get_color("colors", "color_nowx")
-
-
-

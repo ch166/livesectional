0098b042f55f229c64e526d5fe04d640a328a796
Chris Higgins <chris.higgins@alternoc.net>
2023-02-07 20:59:38 -0800

    Deleting pruned code from Attic

--- Attic/getmos06.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -r /NeoSectional/data/GFSMAV &
-sudo wget https://www.weather.gov/source/mdl/MOS/GFSMAV.t06z -P /NeoSectional/data/
-sleep 5
-sudo mv /NeoSectional/data/GFSMAV.t06z /NeoSectional/data/GFSMAV
-
--- Attic/getmos12.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f /NeoSectional/data/GFSMAV
-sudo wget https://www.weather.gov/source/mdl/MOS/GFSMAV.t12z -P /NeoSectional/data/
-sudo mv /NeoSectional/data/GFSMAV.t12z /NeoSectional/data/GFSMAV

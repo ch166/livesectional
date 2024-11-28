ae9931ec5e904525d0676563c729dcb3db0e1cb3
Chris Higgins <chris.higgins@alternoc.net>
2024-11-27 18:01:02 -0800

    Reorganize time/data settings in preparation for rotary switch rewrite

--- update_gpio.py
+++ update_gpio.py
@@ -112,6 +112,47 @@ class UpdateGPIO:
 
         self.read_hardware_settings()
 
+        self.conf.get_int("rotaryswitch", "time_sw0"),
+        self.conf.get_int("rotaryswitch", "data_sw0"),
+
+        # Switch Position Controls
+        # time = Future Offset Hours
+        # data = 0:Metar ; 1:TAF ; 2:MOS
+        switch_positions = {
+            0: {
+                "data": self.conf.get_int("rotaryswitch", "data_sw0"),
+                "time": self.conf.get_int("rotaryswitch", "time_sw0"),
+            },
+            1: {
+                "data": self.conf.get_int("rotaryswitch", "data_sw1"),
+                "time": self.conf.get_int("rotaryswitch", "time_sw1"),
+            },
+            2: {
+                "data": self.conf.get_int("rotaryswitch", "data_sw2"),
+                "time": self.conf.get_int("rotaryswitch", "time_sw2"),
+            },
+            3: {
+                "data": self.conf.get_int("rotaryswitch", "data_sw3"),
+                "time": self.conf.get_int("rotaryswitch", "time_sw3"),
+            },
+            4: {
+                "data": self.conf.get_int("rotaryswitch", "data_sw4"),
+                "time": self.conf.get_int("rotaryswitch", "time_sw4"),
+            },
+            5: {
+                "data": self.conf.get_int("rotaryswitch", "data_sw5"),
+                "time": self.conf.get_int("rotaryswitch", "time_sw5"),
+            },
+            6: {
+                "data": self.conf.get_int("rotaryswitch", "data_sw6"),
+                "time": self.conf.get_int("rotaryswitch", "time_sw6"),
+            },
+            7: {
+                "data": self.conf.get_int("rotaryswitch", "data_sw7"),
+                "time": self.conf.get_int("rotaryswitch", "time_sw7"),
+            },
+        }
+
     def rotary_switch_value(self):
         """Read current value from octal encoder on pins 29/31/33"""
         pin0 = 0
@@ -183,9 +224,6 @@ class UpdateGPIO:
         # * User defined items to be set below - Make changes to config.py, not here *
         # ****************************************************************************
 
-        self.conf = conf
-        self.airport_database = airport_database
-
         # Specific Variables to default data to display if Rotary Switch is not installed.
         # hour_to_display # Offset in HOURS to choose which TAF/MOS to display
         self.hour_to_display = self.conf.get_int("rotaryswitch", "time_sw0")
@@ -202,37 +240,6 @@ class UpdateGPIO:
         self.onminutes = self.conf.get_int("schedule", "onminutes")
         self.offminutes = self.conf.get_int("schedule", "offminutes")
 
-        # set pin 12 to momentary push button to trigger FAA Weather Data update if button is used.
-        GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-        # Setup GPIO pins for rotary switch to choose between Metars, or Tafs and which hour of TAF
-        # Not all the pins are required to be used. If only METARS are desired, then no Rotary Switch is needed.
-        # set pin 0 to ground for METARS
-        GPIO.setup(0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 5 to ground for TAF + 1 hour
-        GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 6 to ground for TAF + 2 hours
-        GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 13 to ground for TAF + 3 hours
-        GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 19 to ground for TAF + 4 hours
-        GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 26 to ground for TAF + 5 hours
-        GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 21 to ground for TAF + 6 hours
-        GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 20 to ground for TAF + 7 hours
-        GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 16 to ground for TAF + 8 hours
-        GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 12 to ground for TAF + 9 hours
-        GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 1 to ground for TAF + 10 hours
-        GPIO.setup(1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # set pin 7 to ground for TAF + 11 hours
-        GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
         # Timer calculations
         self.lights_out = time_(
             self.conf.get_int("schedule", "offhour"),
@@ -244,10 +251,6 @@ class UpdateGPIO:
         self.end_time = self.lights_on
         # Set flag for next round if sleep timer is interrupted by button push.
         self.temp_lights_on = 0
-
-        # Misc Settings
-        # Toggle used for logging when ambient sensor changes from bright to dim.
-        self.ambient_toggle = 0
 
     def update_gpio_flags(self, time_sw, data_sw):
         # Offset in HOURS to choose which TAF to display
@@ -262,54 +265,15 @@ class UpdateGPIO:
         tempsleepon = self.conf.get_int("schedule", "tempsleepon")
 
         while outerloop:
-
             self.read_hardware_settings()
             rotary_switch = self.rotary_switch_value()
 
-            # FIXME: Do an initial import of this data into an array, and then index the array rather than this cumbersome case-style statement.
             # Check if rotary switch is used, and what position it is in. This will determine what to display, METAR, TAF and MOS data.
             # If TAF or MOS data, what time offset should be displayed, i.e. 0 hour, 1 hour, 2 hour etc.
             # If there is no rotary switch installed, then all these tests will fail and will display the defaulted data from switch position 0
-            if rotary_switch == 1:
-                self.update_gpio_flags(
-                    self.conf.get_int("rotaryswitch", "time_sw0"),
-                    self.conf.get_int("rotaryswitch", "data_sw0"),
-                )
-            elif rotary_switch == 2:
-                self.update_gpio_flags(
-                    self.conf.get_int("rotaryswitch", "time_sw1"),
-                    self.conf.get_int("rotaryswitch", "data_sw1"),
-                )
-            elif rotary_switch == 3:
-                self.update_gpio_flags(
-                    self.conf.get_int("rotaryswitch", "time_sw2"),
-                    self.conf.get_int("rotaryswitch", "data_sw2"),
-                )
-
-            elif rotary_switch == 4:
-                self.update_gpio_flags(
-                    self.conf.get_int("rotaryswitch", "time_sw3"),
-                    self.conf.get_int("rotaryswitch", "data_sw3"),
-                )
-            elif rotary_switch == 5:
-                self.update_gpio_flags(
-                    self.conf.get_int("rotaryswitch", "time_sw4"),
-                    self.conf.get_int("rotaryswitch", "data_sw4"),
-                )
-            elif rotary_switch == 6:
-                self.update_gpio_flags(
-                    self.conf.get_int("rotaryswitch", "time_sw5"),
-                    self.conf.get_int("rotaryswitch", "data_sw5"),
-                )
-            elif rotary_switch == 7:
-                self.update_gpio_flags(
-                    self.conf.get_int("rotaryswitch", "time_sw6"),
-                    self.conf.get_int("rotaryswitch", "data_sw6"),
-                )
-            elif rotary_switch == 8:
-                self.update_gpio_flags(
-                    self.conf.get_int("rotaryswitch", "time_sw7"),
-                    self.conf.get_int("rotaryswitch", "data_sw7"),
-                )
+            self.update_gpio_flags(
+                self.switch_positions[rotary_switch]["time"],
+                self.switch_positions[rotary_switch]["data"],
+            )
 
             time.sleep(5)

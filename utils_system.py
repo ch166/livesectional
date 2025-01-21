04174fe9e8d596c60862e8ea5da3aa8f2f40c945
Chris Higgins <chris.higgins@alternoc.net>
2025-01-21 14:50:52 -0800

    Version update / restart handling

--- utils_system.py
+++ utils_system.py
@@ -16,12 +16,15 @@ import conf
 def execute_script(script_path):
     """Try to safely execute scripts."""
 
+    script_result = None
+
     try:
         script_result = subprocess.run(
             script_path, stderr=subprocess.STDOUT, stdout=subprocess.PIPE
         )
-    except OSerror as err:
+    except OSError as err:
         debugging.error(f"script OSError: {script_path} err: {err}")
+        return 1, "None provided"
     if script_result.returncode != 0:
         debugging.error(f"script: {script_path} returned: {script_result.return_code}")
     return script_result.returncode, script_result.stdout
